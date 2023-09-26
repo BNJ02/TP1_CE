7032a17cfb6f0fe913d78ec8ac54ed441874476b
BNJ02 <117920613+BNJ02@users.noreply.github.com>
2023-09-26 16:36:00 +0200

    Question 3 modifiée

--- TP1_CE.X/main.asm
+++ TP1_CE.X/main.asm
@@ -39,14 +39,14 @@ loop:
     btfss GPIO, GP3   ; Vérifie si GP3 est à 1 (bit test skip if set)
     goto gp3_is_clear   ; Saute à "gp3_is_clear" si GP3 est à 0 (LOW)
 
-    ; Si GP3 est à 1 (HIGH), mettre à 0 (LOW) GP0
+    ; Mettre à 0 (LOW) GP0
     bcf GPIO, GP0
     
-    ; Saut vers le début de la boucle (while(1) infinie)
+    ; Saut vers le début de la boucle infinie
     goto loop
 
 gp3_is_clear:
-    ; Si GP3 est à 0 (LOW), mettre à 1 (HIGH) GP0
+    ; Mettre à 1 (HIGH) GP0
     bsf GPIO, GP0
     ; Charge la valeur 250 (en décimal) dans WREG
     movlw 250
@@ -54,6 +54,7 @@ gp3_is_clear:
     movwf temp0
     ; Stocke la valeur de WREG aussi à l'adresse mémoire temp1 (0x12h)
     movwf temp1
+    ; Attend 250ms
     goto timer_250ms
     
 timer_250ms:
@@ -62,7 +63,11 @@ timer_250ms:
     nop
     ; Décrémente la variable temp1
     decfsz temp1, 1
+    ; Si temp1 n'est pas encore à zéro, retourne à la boucle
+    goto timer_250ms
     ; Décrémente la variable temp0
     decfsz temp0, 1
+    ; Si temp0 n'est pas encore à zéro, retourne à la boucle
+    goto timer_250ms
 
 END
