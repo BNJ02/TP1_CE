5b41932fbfa039b001c4d6eddeb0cba3b1c00fa4
BNJ02 <117920613+BNJ02@users.noreply.github.com>
2023-09-26 15:57:35 +0200

    Question 3

--- TP1_CE.X/main.asm
+++ TP1_CE.X/main.asm
@@ -13,38 +13,56 @@ GP1 equ 01h
 GP2 equ 02h
 GP3 equ 03h
 var1 equ 10h
+temp0 equ 11h
+temp1 equ 12h
 
 ; Start
-	ORG 0X000
+    ORG 0X000
 
-	movwf OSCCAL
-	goto main
+    movwf OSCCAL
+    goto main
 
-	ORG 0X020
+    ORG 0X020
 
 ; Set up
 main:	movlw 0CFh
-	option
-	movlw 0F8h
-	tris GPIO
-	goto loop ; Va dans la boucle infinie
+    option
+    movlw 0F8h
+    tris GPIO
+    goto loop ; Va dans la boucle infinie
 
 ; Programme principal
 	
 ; La boucle infinie
 loop:
-	; Lecture de l'état de GP3
-	btfsc GPIO, GP3   ; Vérifie si GP3 est à 0 (bit test skip if clear)
-	goto gp3_is_set   ; Saute à "gp3_is_set" si GP3 est à 1 (HIGH)
+    ; Lecture de l'état de GP3
+    btfss GPIO, GP3   ; Vérifie si GP3 est à 1 (bit test skip if set)
+    goto gp3_is_clear   ; Saute à "gp3_is_clear" si GP3 est à 0 (LOW)
 
-	; Si GP3 est à 0 (LOW), mettre à 1 (HIGH) GP0
-	bsf GPIO, GP0
-
-    gp3_is_set:
-	    ; Si GP3 est à 0 (LOW), mettre à 1 (HIGH) GP0
-	    bcf GPIO, GP0
-	    
+    ; Si GP3 est à 1 (HIGH), mettre à 0 (LOW) GP0
+    bcf GPIO, GP0
+    
     ; Saut vers le début de la boucle (while(1) infinie)
     goto loop
+
+gp3_is_clear:
+    ; Si GP3 est à 0 (LOW), mettre à 1 (HIGH) GP0
+    bsf GPIO, GP0
+    ; Charge la valeur 250 (en décimal) dans WREG
+    movlw 250
+    ; Stocke la valeur de WREG à l'adresse mémoire temp0 (0x11h)
+    movwf temp0
+    ; Stocke la valeur de WREG aussi à l'adresse mémoire temp1 (0x12h)
+    movwf temp1
+    goto timer_250ms
+    
+timer_250ms:
+    nop
+    nop
+    nop
+    ; Décrémente la variable temp1
+    decfsz temp1, 1
+    ; Décrémente la variable temp0
+    decfsz temp0, 1
 
 END
