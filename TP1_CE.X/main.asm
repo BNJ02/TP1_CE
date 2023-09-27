4990a3dec357afb1bffeb6b1b973af8a1efb9ee0
BNJ02 <117920613+BNJ02@users.noreply.github.com>
2023-09-27 16:53:04 +0200

    Question 8 correcte

--- TP1_CE.X/main.asm
+++ TP1_CE.X/main.asm
@@ -13,31 +13,58 @@ GP1 equ 01h
 GP2 equ 02h
 GP3 equ 03h
 var1 equ 10h
+temp0 equ 11h
+temp1 equ 12h
 
 ; Start
-	ORG 0X000
+    ORG 0X000
 
-	movwf OSCCAL
-	goto main
+    movwf OSCCAL
+    goto main
 
-	ORG 0X020
+    ORG 0X020
 
 ; Set up
 main:	movlw 0CFh
-	option
-	movlw 0F8h
-	tris GPIO
-	goto loop ; Va dans la boucle infinie
+    option
+    movlw 0F8h
+    tris GPIO
+    bsf GPIO, GP0
 
 ; Programme principal
-	
 ; La boucle infinie
-loop:
-    ; Lecture de l'état de GP3
-    btfsc GPIO, GP3   ; Vérifie si GP3 est à 0 (bit test skip if clear)
-    bsf GPIO, GP0  
-    bcf GPIO, GP0
-    ; Saut vers le début de la boucle (while(1) infinie)
-    goto loop
+loop: 
+led_on_pressed:
+    btfss GPIO, GP3 ; test de l'entrée 1
+    goto led_on_pressed; Le bouton n'est pas appuyé
+    bsf GPIO, GP0 ; le bouton est appuyé, on allume la led
 
+led_on_released:
+    btfsc GPIO, GP3 ; test de l'entrée 1
+    goto led_on_released ;Le bouton n'est pas relaché...
+    ; Le bouton est relaché
+    
+led_off_pressed: 
+    btfss GPIO, GP3 ; test de l'entrée 1
+    goto led_off_pressed; Le bouton n'est pas appuyé
+    bcf GPIO, GP0 ; le bouton est appuyé, on eteint la led
+
+led_off_released:
+    btfsc GPIO, GP3 ; test de l'entrée 1
+    goto led_off_released ;Le bouton n'est pas relaché...
+    ; Le bouton est relaché  
+    goto loop
+    
+timer_100ms:
+    nop
+    nop
+    nop
+    ; Décrémente la variable temp1
+    decfsz temp1, 1
+    ; Si temp1 n'est pas encore à zéro, retourne à la boucle
+    goto timer_100ms
+    ; Décrémente la variable temp0
+    decfsz temp0, 1
+    ; Si temp0 n'est pas encore à zéro, retourne à la boucle
+    goto timer_100ms
 END
