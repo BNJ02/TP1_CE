45129736afbe5231fcb6fcd9987e8f36564f6ea3
BNJ02 <117920613+BNJ02@users.noreply.github.com>
2023-09-27 10:55:25 +0200

    Question 4 Crash

--- TP1_CE.X/main.asm
+++ TP1_CE.X/main.asm
@@ -29,6 +29,10 @@ main:	movlw 0CFh
     option
     movlw 0F8h
     tris GPIO
+    movlw 0
+    movwf var1
+    ; Mettre à 0 (LOW) GP0
+    bcf GPIO, GP0
     goto loop ; Va dans la boucle infinie
 
 ; Programme principal
@@ -39,71 +43,48 @@ loop:
     btfss GPIO, GP3   ; Vérifie si GP3 est à 1 (bit test skip if set)
     goto gp3_is_clear   ; Saute à "gp3_is_clear" si GP3 est à 0 (LOW)
 
-    ; Mettre à 0 (LOW) GP0
-    bcf GPIO, GP0
-    
     ; Saut vers le début de la boucle infinie
     goto loop
 
+; Si appui sur BP
 gp3_is_clear:
+    ; Changement de valeur pour var1 ("not var1")
+    btfss var1, 0
+    movlw 0
+    btfsc var1, 1
+    movlw 1
+    movwf var1
+    
+    ; Charge la valeur 100 (en décimal) dans WREG (pour 100ms)
+    movlw 100
+    ; Stocke la valeur de WREG à l'adresse mémoire temp0 (0x11h)
+    movwf temp0
+    ; Charge la valeur 250 (en décimal) dans WREG (pour la ms)
+    movlw 250
+    ; Stocke la valeur de WREG aussi à l'adresse mémoire temp1 (0x12h)
+    movwf temp1
+    ; Attend 100ms
+    goto timer_100ms
+    
+    btfss var1, 0
+    ; Mettre à 0 (LOW) GP0
+    bcf GPIO, GP0
+    
+    btfsc var1, 1
     ; Mettre à 1 (HIGH) GP0
     bsf GPIO, GP0
     
-    ;movlw 1
-    ;movwf var1
     
-    ; Charge la valeur 250 (en décimal) dans WREG
-    movlw 250
-    ; Stocke la valeur de WREG à l'adresse mémoire temp0 (0x11h)
-    movwf temp0
-    ; Stocke la valeur de WREG aussi à l'adresse mémoire temp1 (0x12h)
-    movwf temp1
-    
-    ; Attend 250ms
-    goto timer_250ms
-    
-timer_250ms:
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
+timer_100ms:
     nop
     nop
     nop
     ; Décrémente la variable temp1
     decfsz temp1, 1
     ; Si temp1 n'est pas encore à zéro, retourne à la boucle
-    goto timer_250ms
+    goto timer_100ms
     ; Décrémente la variable temp0
     decfsz temp0, 1
     ; Si temp0 n'est pas encore à zéro, retourne à la boucle
-    goto timer_250ms
-
+    goto timer_100ms
 END
